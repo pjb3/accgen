59bceb0a70dac357ebfad25644bc8d59eb33b31a
Paul Barry <mail@paulbarry.com>
2008-12-20 08:36:41 -0500

    Updated explaination of java version

--- accgen.java
+++ accgen.java
@@ -6,10 +6,13 @@ To run this from the command line:
 
   javac accgen.java && java accgen
 
-Note that I think this kind of violates the spirit of the problem,
-because Graham's intent is for you to use a closure and a lambda (a.k.a anonymous function),
-which Java of course has neither of, so we do it with an object.
-Nevertheless, I think this is worth having to compare Object-Oriented vs. Functional styles.
+Note that Paul Graham would probably say that this isn't a valid solution
+because it doesn't use a closure and an anonymous function, instead it uses an object.
+But in everyway, this does solve the problem.  This has a methhod 
+(which is basically thing as function) that returns an object that has a call method
+(ok, not technically a function) that takes a number n and returns it incremented by i.
+I think this is an interesting solution, because depending on your point of view,
+closures are a poor man's objects, or objects are a poor man's clojures.
 
 */
 class accgen {
