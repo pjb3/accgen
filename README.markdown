38a94e0d2e3ba2cfa4a5911aeb90d737ce533c13
Paul Barry <mail@paulbarry.com>
2008-12-22 10:28:16 -0500

    Added clojure solution and README

---
+++ README.markdown
@@ -0,0 +1,7 @@
+This is a collection of implementations of [Paul Graham's Accumulator Generator][accgen]. The problem is:
+
+> The problem: Write a function foo that takes a number n and returns a function that takes a number i, and returns n incremented by i.
+>
+> Note: (a) that's number, not integer, (b) that's incremented by, not plus.
+    
+[accgen]: http://paulgraham.com/accgen.html    
---
+++ accgen.clj
@@ -0,0 +1,19 @@
+;Clojure Implementation of the Accumulator Generator problem
+;see http://www.paulgraham.com/accgen.html for details
+;
+;To run this from the command line:
+;
+;  java -jar clojure.jar accgen.clj
+;
+;This solution is much more verbose than the solution for Ruby or other Lisps,
+;mainly because the problem uses mutable state.  This is understandable because
+;one of the goals of Clojure is to make immutability the default case, 
+;and mutability the exception, which is the opposite of most other languages.
+
+(defn foo [n] 
+  (let [r (ref n)]
+    (fn [i] (dosync (commute r + i)))))
+
+(let [acc (foo 1)] 
+  (doseq [n (range 2 6)] 
+    (println (str n ": " (acc n)))))
